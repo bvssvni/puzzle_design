10411c461d15efb9f058d3744f8362f87000c30d
Sven Nilsen <bvssvni@gmail.com>
2025-12-16 00:32:40 +0100

    Published 0.1.0

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
+
+pub use avalog;
+pub use discrete;
+pub use linear_solver;
+pub use monotonic_solver;
+pub use num;
+pub use piston_mix_economy;
+pub use quickbacktrack;
